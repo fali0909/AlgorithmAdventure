85c681cc1ba10c45d6dd9e225f2d2782b18985a5
fali0909 <faisal20@hotmail.co.uk>
2024-03-20 14:38:23 +0000

    Fixed numbers sorted incorrectly & Added game loop
    Fixed numbers that are placed in order once the player sorted them incorrectly

--- AlgorithmAdventure/Assets/Scripts/QuickSortAlgorithm.cs
+++ AlgorithmAdventure/Assets/Scripts/QuickSortAlgorithm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.Tilemaps.TilemapRenderer;
 
 public class QuickSortAlgorithm : MonoBehaviour {
 
@@ -25,16 +26,19 @@ public class QuickSortAlgorithm : MonoBehaviour {
     private List<int> numbersList;
 
     private bool isInOrder;
+    private List<int> sortedOrder;
 
     private void Start() {
         hasBeenLogged = new bool[ranNum.Length];
         filledBoxesCount = 0;
         numbersList = new List<int>();
         isInOrder = true;
+        sortedOrder = new List<int>();
+
     }
 
     private void Update() {
-        for (int i = 0; i < Mathf.Min(ranNum.Length, boxes.Length); i++) {
+        for (int i = 0; i < ranNum.Length; i++) {
             if (Input.GetMouseButtonUp(0) && !hasBeenLogged[i]) {
                 foreach (Image box in boxes) {
                     if (IsOverlapping(ranNum[i].rectTransform, box.rectTransform)) {
@@ -42,45 +46,68 @@ public class QuickSortAlgorithm : MonoBehaviour {
                             Debug.Log("Integer Value: " + number);
                             hasBeenLogged[i] = true;
                             numbersList.Add(number);
+                            sortedOrder.Add(i); 
                             filledBoxesCount++;
 
-                            if (numbersList.Count > 1 && number < numbersList[numbersList.Count - 2]) {
+                            if (!IsInAscendingOrder(numbersList)) {
                                 isInOrder = false;
                             }
 
                             if (filledBoxesCount == boxes.Length) {
-                                if (isInOrder) {
-                                    Debug.Log("All boxes have been filled. Numbers are in order");
-                                } else {
+                                if (!isInOrder) {
                                     Debug.Log("All boxes have been filled. The numbers are not in order.");
-                                    for (int j = 0; j < ranNum.Length; j++) {
-                                        ranNum[j].transform.position = invisibleBoxes[j].transform.position;
+
+                                    for (int j = 0; j < sortedOrder.Count; j++) {
+                                        int index = sortedOrder[j];
+                                        ranNum[index].transform.position = invisibleBoxes[j].transform.position;
                                     }
+                                    ResetState();
+                                } else {
+                                    Debug.Log("All boxes have been filled. Numbers are in order");
                                 }
                             }
                         }
                     }
                 }
             }
-
-
-            bool IsOverlapping(RectTransform rectTransform1, RectTransform rectTransform2) {
-                Rect rect1 = GetWorldRect(rectTransform1);
-                Rect rect2 = GetWorldRect(rectTransform2);
-
-                return rect1.Overlaps(rect2);
-            }
-
-            Rect GetWorldRect(RectTransform rectTransform) {
-                Vector2 size = Vector2.Scale(rectTransform.rect.size, rectTransform.lossyScale);
-                Rect rect = new Rect(rectTransform.position.x, rectTransform.position.y, size.x, size.y);
-                rect.x -= rectTransform.sizeDelta.x * 0.5f;
-                rect.y -= rectTransform.sizeDelta.y * 0.5f;
-
-                return rect;
-
-            }
         }
     }
 
+    private bool IsInAscendingOrder(List<int> list) {
+        for (int i = 1; i < list.Count; i++) {
+            if (list[i] < list[i - 1]) {
+                return false;
+            }
+        }
+            return true;
+    }
+
+    private void ResetState() {
+    hasBeenLogged = new bool[ranNum.Length];
+    filledBoxesCount = 0;
+    numbersList.Clear();
+    sortedOrder.Clear();
+    isInOrder = true;
+
+    }
+
+
+
+    bool IsOverlapping(RectTransform rectTransform1, RectTransform rectTransform2) {
+        Rect rect1 = GetWorldRect(rectTransform1);
+        Rect rect2 = GetWorldRect(rectTransform2);
+
+        return rect1.Overlaps(rect2);
+    }
+
+    Rect GetWorldRect(RectTransform rectTransform) {
+        Vector2 size = Vector2.Scale(rectTransform.rect.size, rectTransform.lossyScale);
+        Rect rect = new Rect(rectTransform.position.x, rectTransform.position.y, size.x, size.y);
+        rect.x -= rectTransform.sizeDelta.x * 0.5f;
+        rect.y -= rectTransform.sizeDelta.y * 0.5f;
+
+        return rect;
+
+     }
 }
+
