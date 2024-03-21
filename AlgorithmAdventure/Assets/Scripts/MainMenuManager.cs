0d4f89bd354043ac45bb5eb5b61c8a912bc0dd4e
fali0909 <faisal20@hotmail.co.uk>
2024-03-21 08:21:32 +0000

    Added Main Menu & Settings Page

--- AlgorithmAdventure/Assets/Scripts/MainMenuManager.cs
+++ AlgorithmAdventure/Assets/Scripts/MainMenuManager.cs
@@ -6,8 +6,28 @@ using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour {
 
+    [SerializeField] private GameObject mainMenuPage;
+    [SerializeField] private GameObject mapSelection;
+    [SerializeField] private GameObject settingsPage;
+    [SerializeField] private GameObject backSettingsButton;
+
     public void quickSortScene() {
         SceneManager.LoadScene("QuickSortScene");
+    }
+
+    public void MapSelection() {
+        mainMenuPage.SetActive(false);
+        mapSelection.SetActive(true);
+    }
+
+    public void SettingsPage() {
+        settingsPage.SetActive(true);
+        mainMenuPage.SetActive(false);
+    }
+
+    public void BackSettingsButton() {
+        settingsPage.SetActive(false);
+        mainMenuPage.SetActive(true);
     }
 
 }
